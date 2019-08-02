2833bbcd46a2bd5974891085c47334f72c0cdcf1
kirsenkoDanIT <50655852+kirsenkoDanIT@users.noreply.github.com>
2019-08-02 11:57:28 +0300

    Kirsenko (#3)
    * Create config.js
    
    * Create package-lock.json
    
    * Create package.json
    
    * Create server.js
    
    * Create style.css
    
    * Create script.js
    
    * Create head.ejs
    
    * Create index.ejs
    
    * Create layout.ejs
    
    * Update server.js
    
    * Update .gitignore
    
    * Update script.js
    
    * Update server.js
    
    * Update layout.ejs
    
    * Create index.js
    
    * Update server.js

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const expressLayouts = require('express-ejs-layouts')
+const mongoose = require('mongoose')
+const path = require('path')
+
+const config = require('./config.js')
+const indexRoutes = require('./routes/index')
+const app = express()
+
+app.set('view engine', 'ejs')
+app.set("layout extractScripts", true)
+
+app.use(express.urlencoded({
+    extended: true
+}))
+app.use(express.static(path.join(__dirname, 'static')))
+app.use(expressLayouts)
+app.use(indexRoutes);
+
+(async function start() {
+    try {
+        await mongoose.connect(config.DB, {
+            useNewUrlParser: true,
+            useFindAndModify: false
+        })
+        app.listen(config.PORT, () => {
+            console.log(`Connected on port ${config.PORT}`);
+        })
+    } catch (error) {
+        console.log(error);
+    }
+})()
